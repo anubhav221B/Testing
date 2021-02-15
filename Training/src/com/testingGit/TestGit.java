85fa1c59c2352880e53e846e97267fee7e54d4ab
Anubhav Mahajan <anubhavmahajan007@gmail.com>
2021-02-15 13:59:11 +0530

    if added

--- Training/src/com/testingGit/TestGit.java
+++ Training/src/com/testingGit/TestGit.java
@@ -6,7 +6,13 @@ public class TestGit {
 		System.out.println("1. Hello Java");
 		System.out.println("2. Hello Java");
 		System.out.println("3. Hello Java");
-		System.out.println("3. Hello Java");
+		System.out.println("4. Hello Java");
+		int a=10,b=10;
+		if(a==b) {
+			System.out.println("true");
+		}else {
+			System.out.println("false");
+		}
 	}
 
 }
